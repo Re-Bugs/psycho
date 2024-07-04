3442316d77c3f88077a05d5e6bd23d322e22ff3c
Lee SeungWook <leesw1998@gmail.com>
2024-07-04 22:16:10 +0900

    ADD SpringBoot Files

---
+++ SpringBoot/src/main/java/exercise/Psycho_Board/PsychoBoardApplication.java
@@ -0,0 +1,13 @@
+package exercise.Psycho_Board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychoBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychoBoardApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/src/test/java/exercise/Psycho_Board/PsychoBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package exercise.Psycho_Board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychoBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
